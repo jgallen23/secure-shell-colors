3b575ba25f009cbc2a125debc5fe9cb1c7ce428e
Greg Allen <greg@firstandthird.com>
2017-12-31 09:27:23 -0800

    initial commit

---
+++ dark.js
@@ -0,0 +1,22 @@
+term_.prefs_.set('background-color', '#002b36');
+term_.prefs_.set('foreground-color', '#93a1a1');
+term_.prefs_.set('cursor-color', '#93a1a1');
+
+term_.prefs_.set('color-palette-overrides', [
+    '#002b36', //color 0
+    '#dc322f', //color 1
+    '#859900', //color 2
+    '#b58900', //color 3
+    '#268bd2', //color 4
+    '#6c71c4', //color 5
+    '#2aa198', //color 6
+    '#93a1a1', //color 7
+    '#657b83', //color 8
+    '#cb4b16', //color 9
+    '#073642', //color 10
+    '#586e75', //color 11
+    '#839496', //color 12
+    '#eee8d5', //color 13
+    '#d33682', //color 14
+    '#fdf6e3', //color 15
+  ]);
---
+++ light.js
@@ -0,0 +1,22 @@
+term_.prefs_.set('background-color', '#fdf6e3');
+term_.prefs_.set('foreground-color', '#586e75');
+term_.prefs_.set('cursor-color', '#586e75');
+
+term_.prefs_.set('color-palette-overrides', [
+    '#fdf6e3', //color 0
+    '#dc322f', //color 1
+    '#859900', //color 2
+    '#b58900', //color 3
+    '#268bd2', //color 4
+    '#6c71c4', //color 5
+    '#2aa198', //color 6
+    '#93a1a1', //color 7
+    '#657b83', //color 8
+    '#cb4b16', //color 9
+    '#eee8d5', //color 13
+    '#839496', //color 12
+    '#586e75', //color 11
+    '#073642', //color 10
+    '#d33682', //color 14
+    '#002b36', //color 15
+  ]);
